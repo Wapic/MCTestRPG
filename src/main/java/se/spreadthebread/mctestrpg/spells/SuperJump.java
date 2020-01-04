69e088967bedb25e621648817325e1d6c216ab33
Matthew <mbelbin97@gmail.com>
2020-01-04 02:08:27 +0000

    Create SuperJump.java

---
+++ src/main/java/se/spreadthebread/mctestrpg/spells/SuperJump.java
@@ -0,0 +1,22 @@
+package se.spreadthebread.mctestrpg.spells;
+
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+/**
+ * SuperJump
+ */
+public class SuperJump extends Spell {
+
+	public SuperJump() {
+		super(2, "Super Jump", "LLL", 1);
+	}
+
+	@Override
+	public void getEffect(Player player) {
+		player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 5, 5));
+	}
+
+	
+}
